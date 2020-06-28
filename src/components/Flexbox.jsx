00512c24b56fe2634e9eb05af45f35b16577a585
Ranjeet Vishwakarma <ranjeet.vishwakarma@mphasis.com>
2020-06-28 19:19:59 +0530

    flexbox code updated

--- src/components/Flexbox.jsx
+++ src/components/Flexbox.jsx
@@ -3,11 +3,13 @@ import Wrapper from './Hoc/Wrapper'
 const Flexbox = ()=>{
     return (
         <Wrapper>
-            <h1>This is flexbox component</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi dolores dolore iure repudiandae nisi dolorem quo est vitae repellat odio assumenda soluta incidunt maxime, error, doloremque harum ipsum. Nam.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus pariatur corrupti cupiditate cum eligendi temporibus sit distinctio obcaecati cumque. Eum quo rem iusto culpa maxime dolor veritatis a facilis architecto.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil hic dolorum itaque voluptates sed adipisci, deserunt natus fuga modi est vitae consequuntur magnam deleniti libero ullam dolores accusamus eaque unde.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eveniet debitis omnis vitae. Quam aliquid officia vitae nesciunt voluptatibus, vero esse, facilis harum accusamus consequatur rerum non reprehenderit reiciendis asperiores.</p>
+            <div className="flex-container">
+                {[...Array(20)].map((x, i) => (
+                 <div className="flex-item">
+                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                 </div>
+                ))}
+            </div>
         </Wrapper>
     )
 }
