80a2d2ffc34260b5180897632d8aa494a927c221
Ranjeet Vishwakarma <ranjeet.vishwakarma@mphasis.com>
2020-07-02 20:29:27 +0530

    comment removed-

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import Wrapper from './Hoc/Wrapper';
 const Counter =()=> {
-    //const [StudentTile] = useState('This is student component...');
+
   const [counter, setCounter] = useState(0);
-  
-  // Increment handler
+
+  // Increment counter handler
   const incremenetHandler =()=>{
     setCounter(
       counter + 1,
-      //student.push([generateName()])
     );
   }
-  // Decrement handler
+
+  // Decrement counter handler
   let decrementHandler = ()=>{
     setCounter(
       counter - 1,
-      //student.splice(-1, 1)
     );
   }
+  
   return(
     <Wrapper>
         <h3>Cliked Count : {counter}</h3>
